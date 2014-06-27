fd683485ba0c580d42d00f17288bd2b2f5a5f5c8
Vincent Debergue <vde@zengularity.com>
2014-06-27 15:34:09 +0200

    get real data

--- web/scripts/graph.js
+++ web/scripts/graph.js
@@ -4,7 +4,7 @@ var width = 950,
 var zoom = d3.behavior.zoom()
 					  .translate([0, 0])
 					  .scale(1)
-					  .scaleExtent([1, 8])
+					  .scaleExtent([-5, 15])
 					  .on("zoom", zoomed);
 
 var svg = d3.select("#graph").append("svg")
@@ -24,57 +24,46 @@ var projection = d3.geo.mercator()
                        .scale(500000)
                        .translate([width / 2, height / 2]);
 
-var node = container.selectAll("circle.node")
+d3.json("/stop_times.json", function(error, json) {
+  window.data = clean(json);
+
+  var node = container.selectAll("circle.node")
     .data(data.nodesArr)
     .enter()
     .append("g")
     .attr("class", "node")
 
-node.append("svg:circle")
-    .attr("cx", function(d) { return projection([d.lat, d.lon])[0]; })
-    .attr("cy", function(d) { return projection([d.lat, d.lon])[1]; })
-    .attr("r", "10px")
-    .attr("fill", "black");
+  node.append("svg:circle")
+      .attr("cx", function(d) { return projection([d.lat, d.lon])[0]; })
+      .attr("cy", function(d) { return projection([d.lat, d.lon])[1]; })
+      .attr("r", "5px")
+      .attr("fill", "black");
 
-node.append("text")
-    .attr("class", "label")
-    .attr("transform", function(d) {
-      var x = projection([d.lat, d.lon])[0] - 15;
-      var y = (projection([d.lat, d.lon])[1]) - 15;
-      return "translate(" + x + "," + y + ")";
-    })
-    .text(function(d) {return d.name});
+  node.append("text")
+      .attr("class", "label")
+      .attr("transform", function(d) {
+        var x = projection([d.lat, d.lon])[0] - 15;
+        var y = (projection([d.lat, d.lon])[1]) - 15;
+        return "translate(" + x + "," + y + ")";
+      })
+      .text(function(d) {return d.name});
 
-// connecting the dots
-container.selectAll(".line")
-   .data(data.edges)
-   .enter()
-   .append("line")
-   .attr("class", "line")
-   .attr("x1", function(d) { return projection([data.nodes[d.from].lat, data.nodes[d.from].lon])[0]; })
-   .attr("y1", function(d) { return projection([data.nodes[d.from].lat, data.nodes[d.from].lon])[1]; })
-   .attr("x2", function(d) { return projection([data.nodes[d.to].lat, data.nodes[d.to].lon])[0]; })
-   .attr("y2", function(d) { return projection([data.nodes[d.to].lat, data.nodes[d.to].lon])[1]; })
-   .style("stroke", "rgb(6,120,155)");
+  // connecting the dots
+  container.selectAll(".line")
+     .data(data.edges)
+     .enter()
+     .append("line")
+     .attr("class", "line")
+     .attr("x1", function(d) { console.log(d);return projection([data.nodes[d.from].lat, data.nodes[d.from].lon])[0]; })
+     .attr("y1", function(d) { return projection([data.nodes[d.from].lat, data.nodes[d.from].lon])[1]; })
+     .attr("x2", function(d) { return projection([data.nodes[d.to].lat, data.nodes[d.to].lon])[0]; })
+     .attr("y2", function(d) { return projection([data.nodes[d.to].lat, data.nodes[d.to].lon])[1]; })
+     .style("stroke", "rgb(6,120,155)");
+});
 
 function zoomed() {
 	container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  // svg.selectAll("circle.nodes")
  //    .data(nodes)
