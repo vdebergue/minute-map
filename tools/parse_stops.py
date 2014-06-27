e016efc302b769bc39c540cb9a45a9c5cec2c98a
Sébastien Couniot <sco@zengularity.com>
2014-06-27 12:29:22 +0200

    Update parse_stops to parse several files at once

--- tools/parse_stops.py
+++ tools/parse_stops.py
@@ -5,25 +5,29 @@ import csv, hashlib, json, sys
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
-    print 'Please give me a CSV file to parse!'
+    print 'Please give me CSV files to parse!'
     exit()
-  try:
-    with open(sys.argv[1], 'r') as csvfile:
-      stopsreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-      next(stopsreader)
-      stops = {}
 
-      for row in stopsreader:
-        station_hash = hashlib.sha1(row[2]).hexdigest()
-        station_data = stops.get(station_hash, {})
-        if not station_data.has_key('name'):
-          station_data['name'] = row[2]
-          station_data['lon'] = row[5]
-          station_data['lat'] = row[4]
-          station_data['ids'] = []
-        station_data['ids'].append(row[0])
-        stops[station_hash] = station_data
+  stops = {}
 
-      print json.dumps(stops)
-  except IOError:
-    print 'Oops, failed to find or parse the file...'
+  for arg in sys.argv[1:]:
+    try:
+      with open(arg, 'r') as csvfile:
+        stopsreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(stopsreader)
+
+        for row in stopsreader:
+          station_hash = hashlib.sha1(row[2]).hexdigest()
+          station_data = stops.get(station_hash, {})
+          if not station_data.has_key('name'):
+            station_data['name'] = row[2]
+            station_data['lon'] = row[5]
+            station_data['lat'] = row[4]
+            station_data['ids'] = []
+          station_data['ids'].append(row[0])
+          stops[station_hash] = station_data
+    except IOError:
+      print 'Oops, failed to find or parse the file...'
+      pass
+
+  print json.dumps(stops)
